e3007fd2c0ae6d28266c256f059bb18620ecce67
Atanda remilekun <atandaremilekun@gmail.com>
2022-12-28 03:11:10 +0100

    fixed thank you

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -24,13 +24,13 @@ function Thankyou() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="white"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M4.5 12.75l6 6 9-13.5"
             />
           </svg>
