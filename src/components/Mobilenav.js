3cc51d22fecfeb34ed656bbd1e0d57de353669e2
Atanda remilekun <atandaremilekun@gmail.com>
2023-06-25 13:52:14 +0100

    fixed mobilemenu links and navigation

--- src/components/Mobilenav.js
+++ src/components/Mobilenav.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Usemycontext } from "./context/Context";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Mobilenav() {
   const {
+    dispatch,
     store: {
       modalstate: { mobilenav },
     },
   } = Usemycontext();
+
+  const router = useRouter();
+  const changeRoute = (path) => {
+    dispatch({ type: "TOGGLE_MOBILE_NAV" });
+    router.push(path);
+  };
   return (
     <div
       className={`mobilenav px-5 xs:px-6 md:px-10  ${
@@ -23,14 +31,16 @@ function Mobilenav() {
           />
 
           <p className="text-black font-bold tracking-wider mb-4">HEADPHONES</p>
-          <Link href="/headphones">
-            <a className="font-semibold tracking-wider text-sm text-black/70 group">
-              SHOP{" "}
-              <span className="ml-2">
-                <img src="/assets/shared/desktop/icon-arrow-right.svg" />
-              </span>
-            </a>
-          </Link>
+
+          <div
+            className="font-semibold tracking-wider text-sm text-black/70 group"
+            onClick={() => changeRoute("/headphones")}
+          >
+            SHOP{" "}
+            <span className="ml-2">
+              <img src="/assets/shared/desktop/icon-arrow-right.svg" />
+            </span>
+          </div>
         </div>
         <div className="pb-[10px] bg-gray w-full flex flex-col items-center cursor-pointer rounded-[6px]">
           <img
@@ -39,15 +49,17 @@ function Mobilenav() {
             className="w-[120px] -mt-5"
           />
 
-          <p className="text-black font-bold tracking-wider mb-4">HEADPHONES</p>
-          <Link href="/headphones">
-            <a className="font-semibold tracking-wider text-sm text-black/70 group">
-              SHOP{" "}
-              <span className="ml-2">
-                <img src="/assets/shared/desktop/icon-arrow-right.svg" />
-              </span>
-            </a>
-          </Link>
+          <p className="text-black font-bold tracking-wider mb-4">SPEAKERS</p>
+
+          <div
+            className="font-semibold tracking-wider text-sm text-black/70 group"
+            onClick={() => changeRoute("/speakers")}
+          >
+            SHOP{" "}
+            <span className="ml-2">
+              <img src="/assets/shared/desktop/icon-arrow-right.svg" />
+            </span>
+          </div>
         </div>
         <div className=" pb-[10px] bg-gray w-full flex flex-col items-center cursor-pointer rounded-[6px]">
           <img
@@ -56,15 +68,17 @@ function Mobilenav() {
             className="w-[120px] -mt-5"
           />
 
-          <p className="text-black font-bold tracking-wider mb-4">HEADPHONES</p>
-          <Link href="/headphones">
-            <a className="font-semibold tracking-wider text-sm text-black/70 group">
-              SHOP{" "}
-              <span className="ml-2">
-                <img src="/assets/shared/desktop/icon-arrow-right.svg" />
-              </span>
-            </a>
-          </Link>
+          <p className="text-black font-bold tracking-wider mb-4">EARPHONES</p>
+
+          <div
+            className="font-semibold tracking-wider text-sm text-black/70 group"
+            onClick={() => changeRoute("/earphones")}
+          >
+            SHOP{" "}
+            <span className="ml-2">
+              <img src="/assets/shared/desktop/icon-arrow-right.svg" />
+            </span>
+          </div>
         </div>
       </div>
     </div>
